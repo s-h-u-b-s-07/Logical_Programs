b9802c43dc41807b1890071c57f2af60906b46d1
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-05 18:38:40 +0530

    ADDED:Generating a random coupon Numbers

--- src/com/bridgelabz/logicalPrograms/Logical_programs.java
+++ src/com/bridgelabz/logicalPrograms/Logical_programs.java
@@ -5,21 +5,25 @@ import java.util.Scanner;
 public class Logical_programs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number to reversed :: ");
-        int numToReverse = sc.nextInt();
+        System.out.println("Enter the numbers of coupon");
+        int numberOfCoupons = sc.nextInt();
 
-        ReverseNum(numToReverse);
+        int[] couponArray = new int[numberOfCoupons];
+
+        getCouponNumber(couponArray, numberOfCoupons);
+
     }
-    public static void ReverseNum(int numToReverse){
 
-        int reversedNum = 0;
-        while (numToReverse != 0) {
-            int remenderDigit = numToReverse % 10;
-            reversedNum = reversedNum * 10 + remenderDigit;
-            numToReverse /= 10;
+    public static void getCouponNumber(int[] couponArray, int numberOfCoupons) {
+        for (int i = 0; i < numberOfCoupons; i++) {
+            couponArray[i] = (int) (Math.random() * 1000);   //new number is gnerated and stored in array
+            for (int j = 0; j < i; j++) {
+                if (couponArray[j] == couponArray[i]) {         //checks if coupon number is repeated
+                    i = (i - 1);
+                }
+            }
+            System.out.println(i + 1 + ":: Coupon number value is ::" + couponArray[i]);
         }
-
-        System.out.println("Reversed Number: " + reversedNum);
 
     }
 }
