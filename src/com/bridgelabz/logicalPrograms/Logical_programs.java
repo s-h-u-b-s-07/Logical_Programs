4f504d0bd7a72254b5ff3503546a48b8274a4f23
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-05 12:37:15 +0530

    ADDED: reversing the input number

--- src/com/bridgelabz/logicalPrograms/Logical_programs.java
+++ src/com/bridgelabz/logicalPrograms/Logical_programs.java
@@ -5,32 +5,21 @@ import java.util.Scanner;
 public class Logical_programs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number to check Prime Number:: ");
-        int numPrimeCheck = sc.nextInt();
+        System.out.println("Enter a number to reversed :: ");
+        int numToReverse = sc.nextInt();
 
-        PrimeNum(numPrimeCheck);
+        ReverseNum(numToReverse);
     }
+    public static void ReverseNum(int numToReverse){
 
-    public static void PrimeNum(int numPrimeCheck) {
-
-        int halfCondition = 0;
-        int flag = 0;
-
-        halfCondition = numPrimeCheck / 2;
-        if (numPrimeCheck == 0 || numPrimeCheck == 1) {
-            System.out.println(numPrimeCheck + " is not prime number");
-        } else {
-            for (int i = 2; i <= halfCondition; i++) {
-                if (numPrimeCheck % i == 0) {
-                    System.out.println(numPrimeCheck + " is not prime number");
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag == 0) {
-                System.out.println(numPrimeCheck + " is prime number.");
-            }
+        int reversedNum = 0;
+        while (numToReverse != 0) {
+            int remenderDigit = numToReverse % 10;
+            reversedNum = reversedNum * 10 + remenderDigit;
+            numToReverse /= 10;
         }
+
+        System.out.println("Reversed Number: " + reversedNum);
 
     }
 }
