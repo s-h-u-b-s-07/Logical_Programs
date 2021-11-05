322c8652be567cacf1c8940b61503b3f79aeadd4
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-05 12:32:11 +0530

    ADDED: Prime number check

--- src/com/bridgelabz/logicalPrograms/Logical_programs.java
+++ src/com/bridgelabz/logicalPrograms/Logical_programs.java
@@ -5,27 +5,31 @@ import java.util.Scanner;
 public class Logical_programs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number to check perfect number:: ");
-        int perfectCheck = sc.nextInt();
+        System.out.println("Enter a number to check Prime Number:: ");
+        int numPrimeCheck = sc.nextInt();
 
-
-        PerfectCheck(perfectCheck);
+        PrimeNum(numPrimeCheck);
     }
-    public static void PerfectCheck(int perfectCheck) {
 
-        int remainder;
-        int sum = 0;
+    public static void PrimeNum(int numPrimeCheck) {
 
-        for (int i = 1; i < perfectCheck; i++) {
-            remainder = perfectCheck % i;
-            if (remainder == 0) {
-                sum = sum + i;
-            }
-        }
-        if (sum == perfectCheck) {
-            System.out.println("The number is Perfect Number");
+        int halfCondition = 0;
+        int flag = 0;
+
+        halfCondition = numPrimeCheck / 2;
+        if (numPrimeCheck == 0 || numPrimeCheck == 1) {
+            System.out.println(numPrimeCheck + " is not prime number");
         } else {
-            System.out.println("The number is not Perfect Number");
+            for (int i = 2; i <= halfCondition; i++) {
+                if (numPrimeCheck % i == 0) {
+                    System.out.println(numPrimeCheck + " is not prime number");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.println(numPrimeCheck + " is prime number.");
+            }
         }
 
     }
